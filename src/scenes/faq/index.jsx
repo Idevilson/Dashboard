9ba3332faae1cdd86590cb5825ecd636ff2bbd3a
Idevilson.developer <idevilson.developer@gmail.com>
2022-11-08 11:50:30 -0300

    first commit

---
+++ src/scenes/faq/index.jsx
@@ -0,0 +1,91 @@
+import { Box, useTheme, Typography } from "@mui/material";
+import { Header } from "../../components/Header";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcons from "@mui/icons-material/ExpandMore";
+import { tokens } from "../../theme";
+
+
+const FAQ = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+
+    return (
+        <Box m="20px">
+            <Header title="FAQ" subtitle="Frequently Asked Questions Page"/>
+
+            <Accordion defaultExpanded>
+                <AccordionSummary expandIcon={<ExpandMoreIcons />}>
+                    <Typography color={colors.greenAccent[500]} variant="h5">
+                        An Important question
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography >
+                        Lorem ipsom dolor sit amet, consectur adipiscing elit. Suspendisse
+                        meleusada lacus ex, sit amet blandit leo lobordtis get
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+
+            <Accordion defaultExpanded>
+                <AccordionSummary expandIcon={<ExpandMoreIcons />}>
+                    <Typography color={colors.greenAccent[500]} variant="h5">
+                        Another Important question
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography >
+                        Lorem ipsom dolor sit amet, consectur adipiscing elit. Suspendisse
+                        meleusada lacus ex, sit amet blandit leo lobordtis get
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+
+            <Accordion defaultExpanded>
+                <AccordionSummary expandIcon={<ExpandMoreIcons />}>
+                    <Typography color={colors.greenAccent[500]} variant="h5">
+                        Your favorite question
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography >
+                        Lorem ipsom dolor sit amet, consectur adipiscing elit. Suspendisse
+                        meleusada lacus ex, sit amet blandit leo lobordtis get
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+
+            <Accordion defaultExpanded>
+                <AccordionSummary expandIcon={<ExpandMoreIcons />}>
+                    <Typography color={colors.greenAccent[500]} variant="h5">
+                        Some random question
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography >
+                        Lorem ipsom dolor sit amet, consectur adipiscing elit. Suspendisse
+                        meleusada lacus ex, sit amet blandit leo lobordtis get
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+
+            <Accordion defaultExpanded>
+                <AccordionSummary expandIcon={<ExpandMoreIcons />}>
+                    <Typography color={colors.greenAccent[500]} variant="h5">
+                        The final question
+                    </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography >
+                        Lorem ipsom dolor sit amet, consectur adipiscing elit. Suspendisse
+                        meleusada lacus ex, sit amet blandit leo lobordtis get
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+        </Box>
+    );
+};
+
+export default FAQ;
